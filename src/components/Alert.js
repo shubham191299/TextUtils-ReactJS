984c4b11bc857e1f7f07382c27c3b82845795e7c
shubham191299 <shubhamsingh191299@gmail.com>
2024-01-17 17:33:57 +0530

    Added space for Alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -7,7 +7,7 @@ export default function Alert(props) {
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
   return (
-    <div style={{height: '50px'}}>
+    <div style={{height: '58px'}}>
         {props.alert && <div className={`alert alert-${props.alert.type}`} role="alert">
             <strong>{capitalize(props.alert.type)}</strong>: {props.alert.message}
         </div>}
